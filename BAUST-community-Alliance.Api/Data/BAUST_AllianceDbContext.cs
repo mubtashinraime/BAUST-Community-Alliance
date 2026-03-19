7bf2a0453f35d6d664842221e24fe8d719ebda3d
AlHasibRahman <rahmanalhasibur3@gmail.com>
2026-03-20 01:34:21 +0600

    Create a Restfull WebApi using .netCore --version 10.0

---
+++ BAUST-community-Alliance.Api/Data/BAUST_AllianceDbContext.cs
@@ -0,0 +1,16 @@
+using System;
+using BAUST_community_Alliance.Api.Models;
+using Microsoft.EntityFrameworkCore;
+namespace BAUST_community_Alliance.Api.Data;
+
+public class BAUST_AllianceDbContext : DbContext
+{
+    public BAUST_AllianceDbContext(DbContextOptions<BAUST_AllianceDbContext> dbContextOptions)
+    {
+
+    }
+
+    //Dbset properties
+    public DbSet<Content> Contents { get; set; }
+
+}
---
+++ BAUST-community-Alliance.Api/Models/Content.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BAUST_community_Alliance.Api.Models;
+
+public class Content
+{
+    public int id { get; set; }
+    public string heading { get; set; }
+    public string content_about { get; set; }
+    public string detail_description { get; set; }
+    public string image { get; set; }
+    public string? video { get; set; }
+
+}
