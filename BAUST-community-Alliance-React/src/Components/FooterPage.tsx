1e18fdcb4b46b6ed3e655d37605c103f69cf7855
mubtashinraime <Mubtashinraime@gmail.com>
2026-06-02 08:33:54 +0600

    FooterPage.tsx & FooterPage.css file add

---
+++ BAUST-community-Alliance-React/src/Components/FooterPage.tsx
@@ -0,0 +1,14 @@
+// Footer.tsx
+import "./FooterPage.css";
+
+export default function Footer() {
+  return (
+    <footer className="site-footer">
+      <div className="footer-container">
+        <p className="footer-email">Email: rahman@gmail.com</p>
+        <p className="footer-phone">phone: 01571421886</p>
+        <h2 className="footer-contact-title">Contact Us</h2>
+      </div>
+    </footer>
+  );
+}
